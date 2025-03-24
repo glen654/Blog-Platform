9a2f9adbd0a64458c8e3222d1cf932641785760f
glen654 <glenalloy@gmail.com>
2025-03-24 12:08:39 +0530

    debugging user entity implementation

--- user-service/src/main/java/lk/ijse/userservice/entity/impl/UserEntity.java
+++ user-service/src/main/java/lk/ijse/userservice/entity/impl/UserEntity.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "user")
 public class UserEntity implements SuperEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     @Column(unique=true)
